d71d20c05ef2b5fe5fbed875015a892b915fb579
Priyam Aneja <priyamaneja2002@gmail.com>
2025-07-05 11:39:30 +0530

    Tutorial Completed

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -16,6 +16,22 @@ const GsapTimeline = () => {
       duration: 2,
       borderRadius: '100%',
       ease: 'back.inOut'
+    }),
+    timeline.to('#yellow-box', {
+      y: 200, 
+      scale: 2,
+      rotation: 360,
+      borderRadius: '100%',
+      duration: 2,
+      ease: 'back.inOut'
+    }),
+    timeline.to('#yellow-box', {
+      x: 500, 
+      scale: 1,
+      rotation: 360,
+      borderRadius: '8px',
+      duration: 2,
+      ease: 'back.inOut'
     })
   }, []);
 
@@ -53,7 +69,10 @@ const GsapTimeline = () => {
       </p>
 
       <div className="mt-20 space-y-10">
-        <button onClick={() => {}}>Play/Pause</button>
+        <button onClick={() => {
+          if(timeline.paused()) timeline.play();
+          else timeline.pause();
+        }}>Play/Pause</button>
 
         <div id="yellow-box" className="w-20 h-20 bg-yellow-500 rounded-lg" />
       </div>
